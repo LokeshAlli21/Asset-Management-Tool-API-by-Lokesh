73aca2912c2b336710bfe50ccd20f68ddecf6adc
Lokesh <lokeshalli1807@gmail.com>
2026-01-06 16:21:35 +0530

    implement neonDB

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 import express from 'express'
 import { logger } from './middleware/logger.js'
 import assetsRoute from './routes/assets.routes.js'
+import {
+    databaseUrl
+} from './env/env.js'
+import {neon} from '@neondatabase/serverless'
 
 const app = express()
+
+const sql = neon(databaseUrl);
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+app.use(requestHandler)
 
 app.use(express.json())
 app.use(logger)
